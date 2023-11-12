865ce7c3717fc844eae2ceb312ad74839e28497d
MuhammadMubashirjaseem <mubashirjaseem1234@gamil.com>
2023-11-12 18:05:00 +0500

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+// var arr = [[4, 6], [26], [8, 1], [7]];
+// var sum = 0;
+// for(i=0;i<=arr.length;i++){
+//     for(j=0;i<=arr[i].length;j++){
+//     sum+=arr[i][j];
+// }
+// }
+// console.log(sum)
+
+
+
+
+var arr = [[4, 6], [26], [8, 1], [7]];
+
+// var sum = 0;
+
+// for (var i = 0; i < arr.length; i++) {
+//   for (var j = 0; j < arr[i].length; j++) {
+//     sum += arr[i][j];
+//   }
+// }
+
+// console.log("Sum of all numbers:", sum);
+ 
